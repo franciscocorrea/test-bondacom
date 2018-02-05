aae72af663d5889ad680c61447e8d3690b76739b
Franco Correa <anibalfranciscocorrea@gmail.com>
2018-02-05 11:15:41 -0300

    add migrations

---
+++ database/migrations/2018_02_05_123411_create_city_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('zip_code');
+            $table->integer('county_id')->unsigned();
+            $table->foreign('county_id')->references('id')->on('counties');
+            $table->integer('state_id')->unsigned();
+            $table->foreign('state_id')->references('id')->on('states');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::enableForeignKeyConstraints();
+        Schema::dropIfExists('cities');
+        Schema::disableForeignKeyConstraints();
+    }
+}
